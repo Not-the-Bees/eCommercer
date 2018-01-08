be6c54299ac08a53a8ff5de131213a8f57e4b24a
Neobrinoke <alexis.salvador.pro@gmail.com>
2018-01-08 17:30:59 +0100

    auth et migrations

--- database/migrations/2018_01_08_161633_create_orders_history.php
+++ database/migrations/2018_01_08_161633_create_orders_history.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateOrdersHistory extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders_history', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('role');
-            $table->rememberToken();
+            $table->json('cart');
+            $table->float('totalPrice');
+            $table->string('status');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders_history');
     }
 }

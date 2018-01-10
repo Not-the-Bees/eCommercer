5e5eeb1f69ef47ef0202fa6613073fbed08f1ec5
neobrinoke2 <alexis.salvador.pro@gmail.com>
2018-01-10 12:04:33 +0100

    full width

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -12,7 +12,7 @@
     </head>
     <body>
         <nav class="navbar fixed-top navbar-expand-lg navbar-dark">
-            <div class="container">
+            <div class="container-fluid">
                 <img src="{{ asset('img/logo.png') }}" width="15%">
 
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -46,14 +46,12 @@
             </div>
         </nav>
 
-        <main class="container">
+        <main class="container-fluid">
             @yield('content')
         </main>
 
-        <footer class="py-5">
-            <div class="container">
-                <p class="m-0 text-center text-white">Copyright &copy; eCommercer 2018</p>
-            </div>
+        <footer class="container-fluid">
+            <p class="m-0 py-5 text-center text-white">Copyright &copy; eCommercer 2018</p>
         </footer>
 
         <script src="{{ asset('js/jquery.min.js') }}"></script>
